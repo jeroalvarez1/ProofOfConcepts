54c0e78b8e9a5b3f3d517298ea6cb4b853ff06f7
Jerónimo Alvarez <jeroalvarez1@gmail.com>
2023-09-15 12:47:40 -0300

    Funcionamiento basico del sistema terminado. Limpiar datos y generar ids unicos para cada registro.

--- test/test.py
+++ test/test.py
@@ -1,8 +1,8 @@
 from services.ObjectCleaner import ObjectCleaner
 
 dataFrameService = ObjectCleaner()
-dataFrameService.convertCsvToDF(['C:/Users/jeroa/Documents/ProofOfConcepts/test/users_test_1-1.csv', 'C:/Users/jeroa/Documents/ProofOfConcepts/test/users_test_1-2.csv'])
-dataFrameService.convertDFToCsv('C:/Users/jeroa/Documents/ProofOfConcepts/test/new_users_test_1.csv', dataFrameService.mergeRowsOfOneDataFrame(0,'DNI', 'ID1', 'EXTID', 'A23'))
+#dataFrameService.convertCsvToDF(['C:/Users/jeroa/Documents/ProofOfConcepts/test/users_test_1-3.csv'])
+#dataFrameService.convertDFToCsv('C:/Users/jeroa/Documents/ProofOfConcepts/test/users_test_2-3.csv', dataFrameService.mergeRowsOfOneDataFrame(0,'DNI', 'ID3', 'EXTID', 'A23'))
 
 dataFrameService.convertCsvToDF(['C:/Users/jeroa/Documents/ProofOfConcepts/test/users_test_2-1.csv', 'C:/Users/jeroa/Documents/ProofOfConcepts/test/users_test_2-2.csv', 'C:/Users/jeroa/Documents/ProofOfConcepts/test/users_test_2-3.csv'])
 dataFrameService.convertDFToCsv('C:/Users/jeroa/Documents/ProofOfConcepts/test/new_users_test_2.csv', dataFrameService.mergeRowsOfDataFrames('DNI'))
